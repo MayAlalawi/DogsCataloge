9728e4bae2663403423a66947c88e5419b5b1256
MayAlalawi <36498810+MayAlalawi@users.noreply.github.com>
2018-09-15 20:10:08 +0400

    Update listofdogs.py

--- listofdogs.py
+++ listofdogs.py
@@ -20,7 +20,8 @@ session = DBSession()
 
 # Create dummy user
 User1 = User(name="May Ali", email="tinnyTim@udacity.com",
-             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+             picture='https://pbs.twimg.com/profile_images/2671170543/' +
+             '18debd694829ed78203a5a36dd364160_400x400.png')
 session.add(User1)
 session.commit()
 
@@ -36,25 +37,29 @@ brittany_dog = DogsItem(user_id=1, name="Brittany",
                         weight="30-40 pounds",
                         temperament="Bright, Fun-Loving, Upbeat ",
                         description="Sportsmen on both sides of the Atlantic" +
-                        "cherish the agile, energetic Brittany as a stylish and versatile" +
-                        "gundog. Bright and eager at home, and tireless afield, Brittanys"+
-                        " require a lot of exercises, preferably with their favorite humans.",
+                        "cherish the agile, energetic Brittany as a stylish " +
+                        "and versatile gundog. Bright and eager at home," +
+                        " and tireless afield, Brittanys require a lot " +
+                        "of exercises, preferably with their favorite humans.",
                         dogs=group1)
 
 session.add(brittany_dog)
 session.commit()
 
 # English Setter Dog
-english_setter = DogsItem(user_id=1, name="English Setter",
-                        group="Sporting Group",
-                        height="25-27 inches",
-                        weight="65-80 pounds",
-                        temperament="Friendly, Mellow, Merry",
-                        description="The English Setter is a medium sized sporting dog " +
-                        "of sweet temper and show stopping good looks." +
-                        "It is one of the AKS's four British setters created to work on"+
-                        " the distinctly different terrains of England, Ireland, and Scotland.",
-                        dogs=group1)
+english_setter = DogsItem(
+    user_id=1,
+    name="English Setter",
+    group="Sporting Group",
+    height="25-27 inches",
+    weight="65-80 pounds",
+    temperament="Friendly, Mellow, Merry",
+    description="The English Setter is a medium sized sporting dog " +
+    "of sweet temper and show stopping good looks." +
+    "It is one of the AKS's four British setters created to work on" +
+    " the distinctly different terrains of England, Ireland, and Scotland.",
+    dogs=group1
+    )
 
 session.add(english_setter)
 session.commit()
@@ -65,31 +70,36 @@ session.add(group2)
 session.commit()
 
 # Basenji Dog
-basenji_dog = DogsItem(user_id=1, name="Basenji",
-                        group="Hound Group",
-                        height="17 inches",
-                        weight="24 pounds",
-                        temperament="Independent, Smart, Poised",
-                        description="The Basenji, Africa's Barkless Dog, is a compact, " +
-                        "sweet-faced hunter of intelligence and poise.They are unique and" +
-                        "beguiling pets, best for owners who can meet "+
-                        " their exercise needs and the challenge of training this catlike canine.",
-                        dogs=group2)
+basenji_dog = DogsItem(
+    user_id=1,
+    name="Basenji",
+    group="Hound Group",
+    height="17 inches",
+    weight="24 pounds",
+    temperament="Independent, Smart, Poised",
+    description="The Basenji, Africa's Barkless Dog, is a compact, " +
+    "sweet-faced hunter of intelligence and poise.They are unique and" +
+    "beguiling pets, best for owners who can meet " +
+    " their exercise needs and the challenge of training this catlike canine.",
+    dogs=group2
+    )
 
 session.add(basenji_dog)
 session.commit()
 
 # Beagle Dog
-beagle_dog = DogsItem(user_id=1, name="Beagle Dog",
-                        group="Hound Group",
-                        height="13 inches",
-                        weight="under 20 pounds",
-                        temperament="Friendly, Curious, Merry",
-                        description="Not only is the Beagle an excellent hunting dog and loyal " +
-                        "companion, it is also happy-go-lucky, funny, and cute." +
-                        "They were bred to hunt in packs, so they enjoy company and "+
-                        "are generally easygoing.",
-                        dogs=group2)
+beagle_dog = DogsItem(
+    user_id=1,
+    name="Beagle Dog",
+    group="Hound Group",
+    height="13 inches",
+    weight="under 20 pounds",
+    temperament="Friendly, Curious, Merry",
+    description="Not only is the Beagle an excellent hunting dog and loyal " +
+    "companion, it is also happy-go-lucky, funny, and cute.They were " +
+    "bred to hunt in packs, so they enjoy company and are " +
+    "generally easygoing.",
+    dogs=group2)
 
 session.add(beagle_dog)
 session.commit()
@@ -101,31 +111,37 @@ session.add(group3)
 session.commit()
 
 # Boerboel Dog
-boerboel_dog = DogsItem(user_id=1, name="Boerboel",
-                        group="Working Group",
-                        height="24-27 inches",
-                        weight="150-200  pounds",
-                        temperament="Confident, Intelligent, Calm",
-                        description="Boerboels are intimidating but discerning guardians of " +
-                        "home and family who learned their trade while protecting remote South African " +
-                        "homesteads from ferocious predators. They are dominant and confident,"+
-                        " also bright and eager to learn.",
-                        dogs=group3)
+boerboel_dog = DogsItem(
+    user_id=1,
+    name="Boerboel",
+    group="Working Group",
+    height="24-27 inches",
+    weight="150-200  pounds",
+    temperament="Confident, Intelligent, Calm",
+    description="Boerboels are intimidating but discerning guardians of " +
+    "home and family who learned their trade while protecting " +
+    "remote South African homesteads from ferocious predators." +
+    "They are dominant and confident,also bright and eager to learn.",
+    dogs=group3
+    )
 
 session.add(boerboel_dog)
 session.commit()
 
 # Akita Dog
-akita_dog = DogsItem(user_id=1, name="Akita Dog",
-                        group="Working Group",
-                        height="26-28 inches",
-                        weight="100-130  pounds",
-                        temperament="Courageous, Dignified, Profoundly Loyal ",
-                        description="Akita is muscular, double-coated dogs of ancient Japanese lineage " +
-                        "famous for her dignity, courage, and loyalty. In her native land, " +
-                        "she's venerated as family protectors and symbols of good health, "+
-                        "happiness, and long life.",
-                        dogs=group3)
+akita_dog = DogsItem(
+    user_id=1,
+    name="Akita Dog",
+    group="Working Group",
+    height="26-28 inches",
+    weight="100-130  pounds",
+    temperament="Courageous, Dignified, Profoundly Loyal ",
+    description="Akita is muscular, double-coated dogs of ancient " +
+    "Japanese lineage famous for her dignity, courage, and loyalty." +
+    "In her native land, she's venerated as family protectors and" +
+    "symbols of good health, happiness, and long life.",
+    dogs=group3
+    )
 
 session.add(akita_dog)
 session.commit()
@@ -137,31 +153,37 @@ session.add(group4)
 session.commit()
 
 # Australian Terrier
-australian_terrier = DogsItem(user_id=1, name="Australian Terrier",
-                        group="Terrier Group",
-                        height="10-11 inches",
-                        weight="15-20 pounds",
-                        temperament="Affectionate, Courageous, Spirited",
-                        description="The diminutive Australian Terrier is plucky, spirited," +
-                        " and smart how did they fit so much dog into such a bitty package?  " +
-                        "Upbeat and lively, the self assured Aussie approaches life "+
-                        "with plenty of the old-time terrier curiosity and grit.",
-                        dogs=group4)
+australian_terrier = DogsItem(
+    user_id=1,
+    name="Australian Terrier",
+    group="Terrier Group",
+    height="10-11 inches",
+    weight="15-20 pounds",
+    temperament="Affectionate, Courageous, Spirited",
+    description="The diminutive Australian Terrier is plucky, spirited," +
+    " and smart how did they fit so much dog into such a bitty package?  " +
+    "Upbeat and lively, the self assured Aussie approaches life " +
+    "with plenty of the old-time terrier curiosity and grit.",
+    dogs=group4
+    )
 
 session.add(australian_terrier)
 session.commit()
 
 # Bull Terrier
-bull_terrier = DogsItem(user_id=1, name="Bull Terrier",
-                        group="Terrier Group",
-                        height="21-22 inches",
-                        weight="50-70  pounds",
-                        temperament="Playful, Charming, Mischievous",
-                        description="Among the most comical and mischievous citizens of dogdom, the" +
-                        "Bull Terrier is playful and endearing, sometimes stubborn, but always devoted." +
-                        "These unique eggheads are exuberant, muscular companions who thrive on "+
-                        "affection and exercise.",
-                        dogs=group4)
+bull_terrier = DogsItem(
+    user_id=1,
+    name="Bull Terrier",
+    group="Terrier Group",
+    height="21-22 inches",
+    weight="50-70  pounds",
+    temperament="Playful, Charming, Mischievous",
+    description="Among the most comical and mischievous citizens of " +
+    "dogdom, the Bull Terrier is playful and endearing, sometimes " +
+    "stubborn, but always devoted.These unique eggheads are exuberant, " +
+    "muscular companions who thrive on affection and exercise.",
+    dogs=group4
+    )
 
 session.add(bull_terrier)
 session.commit()
